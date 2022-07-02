6a46b6eee17c6f56f16cd691ef1b342b41c1852d
LMoura94 <leonardo.moura.ismart@gmail.com>
2022-07-02 18:35:49 -0300

    Header and Footer start

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php require_once "meta.php"; ?>
+	<title>Portfolio by Leonardo Moura</title>
+</head>
+<body>
+	<header>
+		<?php require_once "header.php"; ?>
+	</header>
+	<footer>
+		<?php require_once "footer.php"; ?>
+	</footer>
+</body>
+</html>
---
+++ meta.php
@@ -0,0 +1,6 @@
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap-grid.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="css/style.css">
