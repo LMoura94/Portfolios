4c9f84fc68f90e495cbdc6843d76d81e1bfc35c0
LMoura94 <leonardo.moura.ismart@gmail.com>
2022-07-18 10:45:43 -0300

    style

--- index.php
+++ index.php
@@ -9,16 +9,20 @@
 	<header>
 			<?php require_once "header.php"; ?>
 	</header>
-	<div class="container-fluid row mt-3">
+	<div class="container-fluid pagination mt-3 w-75">
 		
-		<article class="col-lg ">
-			<section class="text-center">
+		<article class="col-lg w-50">
+			<section class="text-body">
+				
 				<h1>
 					Portfólio Projeto
 				</h1>
-				<p>Meu portfólio esta sendo desenvolvido de maneira gradual,juntando os outros de Wordpress,Webflow e Variados, se por acaso você estiver e não encontrar nada por ir até:
-					https://www.linkedin.com/in/leonardo-moura-a997b4200/
+				<p>Meu portfólio esta sendo desenvolvido de maneira gradual, juntando os outros de Wordpress,Webflow e Variados, se por acaso você estiver e não encontrar nada por ir até:
 				</p>
+				<p>
+					<a href="https://www.linkedin.com/in/leonardo-moura-a997b4200/" class="page-link">Acesse meu perfil no LinkedIn</a>
+				</p>
+				
 			</section>
 		</article>
 		<?php require_once "sidebar.php"; ?>
