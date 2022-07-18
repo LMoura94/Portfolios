25783effa310f460964364242b1e3a9a34178e48
LMoura94 <leonardo.moura.ismart@gmail.com>
2022-07-18 09:43:24 -0300

    input pages

---
+++ Contato.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<?php require_once "head.php"; ?>
+	<title>Portfolios by Leonardo Moura</title>
+</head>
+<body>
+	<header>
+			<?php require_once "header.php"; ?>
+	</header>
+	<div class="container-fluid row mt-3">
+		
+		<article class="col-lg ">
+			<section class="text-center">
+				<h1>
+				</h1>
+				<p>
+				</p>
+			</section>
+		</article>
+		<?php require_once "sidebar.php"; ?>
+	</div>
+	<footer>
+			<?php require_once "footer.php"; ?>
+	</footer>
+
+</body>
+</html>
